568f0883295c44811a0c31cccf197a731e33eafd
Zhengjie Yang <3282892961@qq,com>
2025-09-28 10:43:00 +0800

    新增用户下订单业务

--- testcases/createOrder/test_create_order_scenario.py
+++ testcases/createOrder/test_create_order_scenario.py
@@ -7,7 +7,7 @@ from unit_tools.handle_data.yaml_handler import read_yaml
 @allure.feature('电子商务管理系统用户下订单业务管理')
 class TestCreateOrderScenario:
     @pytest.mark.parametrize('api_info',read_yaml('./data/createOrderScenario.yaml'))
-    @allure.dynamic.title('用户下订单流程')
+    @allure.story('用户下订单流程')
     def test_create_order_scenario(self,api_info):
         allure.dynamic.title(api_info['baseInfo']['api_name'])
         RequestsBase().execute_test_case(api_info)
